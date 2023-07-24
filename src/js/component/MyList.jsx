4fce10947fc883541cff92c15181111586ce9edf
Yúliya Bundur <123424819+yuliyabundur@users.noreply.github.com>
2023-07-24 21:59:27 +0000

    ¨Ready¨

--- src/js/component/MyList.jsx
+++ src/js/component/MyList.jsx
@@ -14,8 +14,10 @@ function MyList() {
 		}
 	};
 
-	const deleteTask = (task) => {
-		setTask(task.filter((t) => t !== task));
+	const deleteTask = (index) => {
+		const updatedTasks = [...task];
+		updatedTasks.splice(index, 1);
+		setTask(updatedTasks);
 	};
 
 	const handleKeyPress = (e) => {
@@ -42,12 +44,12 @@ function MyList() {
 
 
 			<p className="task-list">
-				{task.map((task) => (
-					<p className="task-item">
+				{task.map((task, index) => (
+					<p className="task-item" key={index}>
 
 						<span>{task}</span>
 
-						<button className="delete-button" onClick={() => deleteTask(task)}>
+						<button className="delete-button" onClick={() => deleteTask(index)}>
 							<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash3-fill" viewBox="0 0 16 16">
 								<path d="M11 1.5v1h3.5a.5.5 0 0 1 0 1h-.538l-.853 10.66A2 2 0 0 1 11.115 16h-6.23a2 2 0 0 1-1.994-1.84L2.038 3.5H1.5a.5.5 0 0 1 0-1H5v-1A1.5 1.5 0 0 1 6.5 0h3A1.5 1.5 0 0 1 11 1.5Zm-5 0v1h4v-1a.5.5 0 0 0-.5-.5h-3a.5.5 0 0 0-.5.5ZM4.5 5.029l.5 8.5a.5.5 0 1 0 .998-.06l-.5-8.5a.5.5 0 1 0-.998.06Zm6.53-.528a.5.5 0 0 0-.528.47l-.5 8.5a.5.5 0 0 0 .998.058l.5-8.5a.5.5 0 0 0-.47-.528ZM8 4.5a.5.5 0 0 0-.5.5v8.5a.5.5 0 0 0 1 0V5a.5.5 0 0 0-.5-.5Z" />
 							</svg>
